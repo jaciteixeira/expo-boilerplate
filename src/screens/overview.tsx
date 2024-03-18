da17c4a263e5c3b2dca2e6ecb21f2f4e54078477
Yarlei Cruz <yarleicruz1998@gmail.com>
2024-03-17 22:24:12 -0300

    tela login finalizado

--- src/screens/overview.tsx
+++ src/screens/overview.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View, Image } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { Icon } from 'react-native-paper';
 // import { useState } from 'react';
@@ -17,36 +17,53 @@ export default function Overview() {
       <Text style={styles.header}>Log in</Text>
       <View style={styles.input}>
         <Icon source="email-outline" size={20} color="gray" />
-        <TextInput placeholder="Your email" />
+        <TextInput placeholder="Your email" 
+        style={styles.textInput}/>
       </View>
       <View style={styles.input}>
         <Icon source="key" size={20} color="gray" />
-        <TextInput placeholder="Your password" />
+        <TextInput placeholder="Your password" 
+        style={styles.textInput}/>
       </View>
       <TouchableOpacity
-        onPress={() => alert('indo pra tela de dashboard')}
+        onPress={() => navigation.navigate('Details', { name: 'Dan' })}
         style={styles.loginButton}>
         <Text style={styles.loginButtonText}>Log in</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => alert('hello')}>
+      <TouchableOpacity onPress={() => alert('forgot password')}>
         <Text style={styles.forgotText}>Forgot password</Text>
       </TouchableOpacity>
 
       <View style={styles.dividerContainer}>
         <View style={styles.divider} />
-        <Text style={styles.dividerText}>OR</Text>
+        <Text style={styles.dividerText}>or</Text>
         <View style={styles.divider} />
       </View>
       <View style={styles.loginWithSocial}>
         <TouchableOpacity
-          onPress={() => alert('indo pra tela de dashboard')}
-          >
-          <Text style={styles.googleButton}>Google</Text>
+          onPress={() => alert('login com google')}
+          style={styles.loginWithSocialButton}>
+          <Image
+            source={require('../../assets/google.png')}
+            style={styles.image}
+          />
+          <Text>Google</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => alert('indo pra tela de dashboard')}
-          >
-          <Text style={styles.facebookButton}>Facebook</Text>
+          onPress={() => alert('login com facebook')}
+          style={styles.loginWithSocialButton}>
+          <Image
+            source={require('../../assets/facebook.png')}
+            style={styles.image}
+          />
+          <Text>Facebook</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.createAccount}>
+        <Text style={{color:'#969AB8'}}>Don’t have an account?  </Text>
+        <TouchableOpacity 
+        onPress={() => alert('indo pra tela de inscrição')}>
+          <Text style={styles.sign}>Sign Up</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -69,15 +86,18 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'gray',
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 8,
     marginBottom: 10,
     flexDirection: 'row',
     alignItems: 'center',
   },
+  textInput:{
+    marginLeft: 10
+  },
   loginButton: {
-    backgroundColor: 'blue',
+    backgroundColor: '#0062FF',
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 8,
     marginTop: 10,
     alignItems: 'center',
   },
@@ -105,27 +125,34 @@ const styles = StyleSheet.create({
   dividerText: {
     marginHorizontal: 10,
   },
-  googleButton:{
-    marginHorizontal: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    marginTop: 10,
-    alignItems: 'center',
-  },
-  facebookButton:{
-    marginHorizontal: 10,
-    borderWidth: 1,
-    borderColor: 'gray',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-    alignItems: 'center',
-  },
   loginWithSocial:{
     flexDirection: 'row', 
-    justifyContent: 'center',
-  }
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+  loginWithSocialButton:{
+    width:'49%',
+    flexDirection:'row',
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  createAccount:{
+    marginTop: 20,
+    flexDirection:'row',
 
+  },
+  image:{ 
+    width: 20, 
+    height: 20,
+    marginRight: 10
+  },
+  sign:{
+    fontWeight: 'bold',
+    color: '#0062FF'
+  }
 });
